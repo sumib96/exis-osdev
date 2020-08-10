831af626714bb83c7a07f1c5958f78c3f319f1aa
Sumib96 <48891955+sumib96@users.noreply.github.com>
2020-08-10 16:52:19 +0200

    Build test kernel

--- Source/Exis/Kernel.c
+++ Source/Exis/Kernel.c
@@ -1,8 +1,25 @@
+typedef unsigned int UINT32;
+
+void SetPixel (int Y, int X, UINT32 Color)
+{
+	char *memory = (char*)0x90000000;
+	memory[4 * (Y * 800 + X)] = Color;
+}
+
 void main(void)
 {
-	int x = 10;
-	x++;
+	char *memory = (char*)0x90000000;
 	
-	int* memory = (int*)0x1000;
-	memory[0] = x;
+	for (int i = 0; i < 1920000; i++)
+	{
+		memory[i] = 0;
+	}
+
+	for (int i = 0; i < 800; i++)
+	{
+		for (int l = 0; l < 600; l++)
+		{
+			SetPixel(l, i, 300);
+		}
+	}
 }
--- Source/Exis/build.bat
+++ Source/Exis/build.bat
@@ -1,2 +1,3 @@
-set Options = 
-set Sources =
+clang -c -m64 --target=i686-pc-none-elf -Wall -Werror -ffreestanding -fno-pic -fno-builtin -nostdlib -nostdinc -Ikernel -O2 -flto -g Kernel.c
+ld.lld --strip-all -nostdlib -T link.ld Kernel.o -o Kernel.elf
+copy "C:\Users\MainDev\Documents\GitHub\exis-osdev\Source\Exis\Kernel.elf" "C:\Users\MainDev\Desktop\VisualUefi-master\samples\x64\Release" /y
